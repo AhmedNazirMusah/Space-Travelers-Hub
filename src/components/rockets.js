4efb6cd7e682842f5b6db602b91385a2a649e01d
ahmednazirmusah <ahmed999nazir@gmail.com>
2022-11-15 13:18:31 +0000

    added basic structure for rockets and fetched and displayed rockets without styling

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import { useEffect, React } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rockets';
+import Rocket from './rocket';
 
-const Rockets = () => (
-  <div>
-    <h2>Rockets</h2>
-  </div>
-);
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!rockets.length > 0) {
+      dispatch(fetchRockets());
+    }
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {rockets.map((rocket) => (
+          <Rocket key={rocket.id} rocket={rocket} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
