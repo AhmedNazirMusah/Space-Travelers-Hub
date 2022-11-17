fcec45f4866c5234b56a73278e31775f94156077
kaizipaul <kaizipaul@gmail.com>
2022-11-17 11:28:01 +0300

    Added Reserved missions to MyProfile components

--- src/components/myProfile.js
+++ src/components/myProfile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { cancelBooking } from '../redux/rockets/rockets';
+import { cancelMission } from '../redux/missions/missions';
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -17,6 +18,18 @@ const Profile = () => {
       </div>
     ));
 
+  const missionState = useSelector((state) => state.missionsReducer);
+  const reservedMissions = missionState.filter((mission) => mission.reserved).length === 0
+    ? <p>No Missions reserved </p>
+    : missionState.filter((mission) => mission.reserved).map((mission) => (
+      <div key={mission.id}>
+        <div>
+          <h2>{mission.mission_name}</h2>
+        </div>
+        <button type="button" onClick={() => dispatch(cancelMission(mission.id))}>Cancel Reservation</button>
+      </div>
+    ));
+
   return (
     <div>
       <div className="profile">
@@ -25,6 +38,14 @@ const Profile = () => {
           <ul>
             <li>
               {reservedRockets}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>My Missions</h2>
+          <ul>
+            <li>
+              {reservedMissions}
             </li>
           </ul>
         </div>
